e308b37b776bd9674d87971095bc4979dc3b3ca8
canghun13 <41933428+canghun13@users.noreply.github.com>
2026-06-01 13:51:12 +0900

    Add files via upload

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -14,14 +14,6 @@ const GA_ID = 'G-QZT8PVVY5E';
   document.head.appendChild(s);
 })();
 
-// Google AdSense
-(function () {
-  var a = document.createElement('script');
-  a.async = true;
-  a.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5592663499707350';
-  a.crossOrigin = 'anonymous';
-  document.head.appendChild(a);
-})();
 window.dataLayer = window.dataLayer || [];
 function gtag() { dataLayer.push(arguments); }
 gtag('js', new Date());
